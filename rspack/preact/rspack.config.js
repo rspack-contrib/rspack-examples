57ca4bf05ffb639cbbdf7503a9e8fde1ad96940c
hardfist <yangjianzju@gmail.com>
2024-01-11 22:57:50 +0800

    chore: add preact example (#10)

---
+++ rspack/preact/rspack.config.js
@@ -0,0 +1,59 @@
+const rspack = require('@rspack/core');
+const dev = process.env.NODE_ENV === 'development';
+/** @type {import('@rspack/cli').Configuration} */
+const config = {
+  entry: {
+    main: './src/index.jsx',
+  },
+  resolve: {
+    extensions: ['...', '.ts', '.tsx', '.jsx'],
+    alias: {
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat', // Must be below test-utils
+      'react/jsx-runtime': 'preact/jsx-runtime',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        use: {
+          loader: 'builtin:swc-loader',
+          options: {
+            sourceMap: true,
+            jsc: {
+              parser: {
+                syntax: 'ecmascript',
+                jsx: true,
+              },
+              externalHelpers: true,
+              preserveAllComments: false,
+              transform: {
+                react: {
+                  runtime: 'automatic',
+                  pragma: 'h',
+                  pragmaFrag: 'Fragment',
+                  throwIfNamespace: true,
+                  useBuiltins: false,
+                },
+              },
+            },
+          },
+        },
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.(png|svg|jpg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: './index.html',
+    }),
+		dev && new rspack.HotModuleReplacementPlugin()
+  ].filter(Boolean),
+};
+module.exports = config;
