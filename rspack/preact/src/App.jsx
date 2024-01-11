34a027ee4a4ec0073b642aa62ec5c9de98af8612
hardfist <yangjianzju@gmail.com>
2024-01-11 22:41:32 +0800

    chore: add preact example

---
+++ rspack/preact/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(1);
+  useEffect(() => {
+    setInterval(() => {
+      setCount(x => x+1);
+    },1000)
+  },[])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
+          count: {count}
+        </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
