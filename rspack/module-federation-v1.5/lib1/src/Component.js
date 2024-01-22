af0e9e8d87d82d24bac151b535a48fb925267cc1
hardfist <yangjianzju@gmail.com>
2024-01-22 23:34:25 +0800

    chore: add counter to show fast-refresh is working (#15)

--- rspack/module-federation-v1.5/lib1/src/Component.js
+++ rspack/module-federation-v1.5/lib1/src/Component.js
@@ -1,17 +1,27 @@
 import { formatRelative, subDays } from "date-fns";
+import { useEffect, useState } from "react";
 // date-fns is a shared module, but used as usual
 // exposing modules act as async boundary,
 // so no additional async boundary need to be added here
 // As data-fns is an shared module, it will be placed in a separate file
 // It will be loaded in parallel to the code of this module
 
-const Component = ({ locale }) => (
-	<div style={{ border: "5px solid darkblue" }}>
-		<p>I'm a Component exposed from container B!</p>
-		<p>
-			Using date-fn in Remote:{" "}
-			{formatRelative(subDays(new Date(), 2), new Date(), { locale })}
-		</p>
-	</div>
-);
+const Component = ({ locale }) => {
+  let [cnt, setCnt] = useState(0);
+  useEffect(() => {
+	setInterval(() => {
+		setCnt(x => x+1);
+	}, 1000)
+  },[])
+  return (
+    <div style={{ border: "5px solid darkblue" }}>
+	  <div>cnt: {cnt}</div>
+      <p>I'm a Component exposed from container B!</p>
+      <p>
+        Using date-fn in Remote:{" "}
+        {formatRelative(subDays(new Date(), 2), new Date(), { locale })}
+      </p>
+    </div>
+  );
+};
 export default Component;
