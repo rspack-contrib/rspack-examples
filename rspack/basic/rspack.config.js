c54eea0cc59dab58654a093f412e67069cf7735f
hardfist <yangjian.fe@bytedance.com>
2024-01-26 16:09:23 +0800

    chore: add ts and mts config example

--- rspack/basic/rspack.config.js
+++ rspack/basic/rspack.config.js
@@ -1,11 +1,12 @@
 const rspack = require("@rspack/core");
 /** @type {import('@rspack/cli').Configuration} */
 const config = {
-	entry: "./src/index.js",
-	plugins: [
-		new rspack.HtmlRspackPlugin({
-			template: "./index.html"
-		})
-	]
+  context: __dirname,
+  entry: "./src/index.js",
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: "./index.html",
+    }),
+  ],
 };
 module.exports = config;
