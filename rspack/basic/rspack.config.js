88eab6c71ab9de10304b00240bcae8d26ef7128a
Fy <1114550440@qq.com>
2024-01-29 18:26:59 +0800

    Revert: revert ts and mts config example" (#18)
    This reverts commit c54eea0cc59dab58654a093f412e67069cf7735f.

--- rspack/basic/rspack.config.js
+++ rspack/basic/rspack.config.js
@@ -1,12 +1,11 @@
 const rspack = require("@rspack/core");
 /** @type {import('@rspack/cli').Configuration} */
 const config = {
-  context: __dirname,
-  entry: "./src/index.js",
-  plugins: [
-    new rspack.HtmlRspackPlugin({
-      template: "./index.html",
-    }),
-  ],
+	entry: "./src/index.js",
+	plugins: [
+		new rspack.HtmlRspackPlugin({
+			template: "./index.html"
+		})
+	]
 };
 module.exports = config;
