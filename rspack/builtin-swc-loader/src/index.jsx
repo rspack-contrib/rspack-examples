73bc973970647a4500c633a72524a390295d7cf6
hardfist <yangjian.fe@bytedance.com>
2024-02-07 15:11:33 +0800

    chore: add @swc/helpers alias in monorepo

--- rspack/builtin-swc-loader/src/index.jsx
+++ rspack/builtin-swc-loader/src/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {answer} from 'lib3';
+console.log('answer:',answer());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
