a8be8b1541469c0fba3f35d6fef67b8ddce9a90c
hardfist <yangjianzju@gmail.com>
2024-02-07 23:27:28 +0800

    chore: add swc helpers alias in monorepo (#23)
    * chore: add @swc/helpers alias in monorepo
    
    * chore: fix remove unncessary flags

--- rspack/builtin-swc-loader/rspack.config.js
+++ rspack/builtin-swc-loader/rspack.config.js
@@ -1,55 +1,62 @@
 const rspack = require("@rspack/core");
+const path = require("path");
 /** @type {import('@rspack/cli').Configuration} */
 const config = {
-	entry: {
-		main: "./src/index.jsx"
-	},
-	resolve: {
-		extensions: ["...", ".jsx"]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx$/,
-				use: {
-					loader: "builtin:swc-loader",
-					options: {
-						// Enable source map
-						sourceMap: true,
-						jsc: {
-							parser: {
-								syntax: "ecmascript",
-								jsx: true
-							},
-							externalHelpers: true,
-							preserveAllComments: false,
-							transform: {
-								react: {
-									runtime: "automatic",
-									pragma: "React.createElement",
-									pragmaFrag: "React.Fragment",
-									throwIfNamespace: true,
-									useBuiltins: false
-								}
-							}
-						}
-					}
-				},
-				type: "javascript/auto"
-			},
-			{
-				test: /\.(png|svg|jpg)$/,
-				type: "asset/resource"
-			}
-		]
-	},
-	optimization: {
-		minimize: false // Disabling minification because it takes too long on CI
-	},
-	plugins: [
-		new rspack.HtmlRspackPlugin({
-			template: "./index.html"
-		})
-	]
+  entry: {
+    main: "./src/index.jsx",
+  },
+  resolve: {
+    extensions: ["...", ".jsx"],
+    alias: {
+      "@swc/helpers": path.dirname(
+        require.resolve("@swc/helpers/package.json")
+      ),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        use: {
+          loader: "builtin:swc-loader",
+          options: {
+            // Enable source map
+            sourceMap: true,
+            target: "es5",
+            jsc: {
+              parser: {
+                syntax: "ecmascript",
+                jsx: true,
+              },
+              externalHelpers: true,
+              preserveAllComments: false,
+              transform: {
+                react: {
+                  runtime: "automatic",
+                  pragma: "React.createElement",
+                  pragmaFrag: "React.Fragment",
+                  throwIfNamespace: true,
+                  useBuiltins: false,
+                },
+              },
+            },
+          },
+        },
+        type: "javascript/auto",
+      },
+      {
+        test: /\.(png|svg|jpg)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  optimization: {
+    minimize: false, // Disabling minification because it takes too long on CI
+  },
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: "./index.html",
+    }),
+  ],
 };
 module.exports = config;
--- rspack/nest-alias/rspack.config.js
+++ rspack/nest-alias/rspack.config.js
@@ -1,18 +1,13 @@
-const {defineConfig } = require('@rspack/cli');
-const path = require('path');
+const { defineConfig } = require("@rspack/cli");
+const path = require("path");
 module.exports = defineConfig({
   entry: {
-    main: './src/index.ts',
-  },
-  experiments: {
-    rspackFuture: {
-      newResolver:true
-    }
+    main: "./src/index.ts",
   },
   resolve: {
     tsConfig: {
-      references: 'auto',
-      configFile:path.resolve(__dirname,'./tsconfig.json')
-    }
-  }
+      references: "auto",
+      configFile: path.resolve(__dirname, "./tsconfig.json"),
+    },
+  },
 });
