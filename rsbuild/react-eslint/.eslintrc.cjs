bebd6ce44c667774fb319586296775c2f6e1ae16
neverland <jait.chen@foxmail.com>
2024-02-28 13:48:15 +0800

    Add rsbuild-react-eslint example (#29)

---
+++ rsbuild/react-eslint/.eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: { browser: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+};
