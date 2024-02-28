4f8cd7e1c06fbabf0905625723085146dbaf170d
neverland <chenjiahan.jait@bytedance.com>
2024-02-28 16:03:30 +0800

    feat(rsbuild): update react-eslint example

--- rsbuild/react-eslint/rsbuild.config.ts
+++ rsbuild/react-eslint/rsbuild.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "@rsbuild/core";
+import { pluginEslint } from "@rsbuild/plugin-eslint";
 import { pluginReact } from "@rsbuild/plugin-react";
-import ESLintPlugin from "eslint-webpack-plugin";
 
 export default defineConfig({
-  plugins: [pluginReact()],
-  tools: {
-    rspack: {
-      plugins: [
-        new ESLintPlugin({
-          extensions: ["js", "jsx", "ts", "tsx"],
-        }),
-      ],
-    },
-  },
+  plugins: [pluginReact(), pluginEslint()],
 });
