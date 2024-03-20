8b3c403da1cb6d0098958ff8b4a9282baf660715
chenjiahan <chenjiahan.jait@bytedance.com>
2024-03-20 21:40:56 +0800

    Add rsbuild-react-tailwindcss example

---
+++ rsbuild/react-tailwindcss/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen text-center">
+      <h1 className="text-6xl font-bold leading-normal">Rsbuild with React</h1>
+      <p className="text-lg font-normal text-gray-500 leading-normal">
+        Start building amazing things with Rsbuild.
+      </p>
+    </div>
+  );
+};
+
+export default App;
