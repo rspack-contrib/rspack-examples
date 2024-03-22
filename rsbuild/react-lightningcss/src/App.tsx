de1a1ee995a2a59ebedef489f90c1a37523f3f9b
Soon <a1149625757@126.com>
2024-03-22 14:32:23 +0800

    feat: add react-lightningcss (#38)
    Co-authored-by: neverland <jait.chen@foxmail.com>

---
+++ rsbuild/react-lightningcss/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <h1>Lightning CSS</h1>
+
+      <p className="blue">&lt;link&gt;: This should be #00f</p>
+
+      <p className="color-mix">&lt;link&gt;: This should be #706a43</p>
+    </div>
+  );
+};
+
+export default App;
