81d6deecd15dcab88d0647ef5e8932b67552293c
Fy <1114550440@qq.com>
2024-04-10 20:53:39 +0800

    feat: update css-extract example (#46)

---
+++ rspack/react-with-extract-css/rspack.config.js
@@ -0,0 +1,73 @@
+const rspack = require("@rspack/core");
+const path = require("path");
+
+/** @type {import('@rspack/cli').Configuration} */
+const config = {
+	context: __dirname,
+	mode: "development",
+	entry: {
+		main: ["./src/index.jsx"]
+	},
+	resolve: {
+		extensions: ["...", ".ts", ".tsx", ".jsx"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				use: {
+					loader: "builtin:swc-loader",
+					options: {
+						sourceMap: true,
+						jsc: {
+							parser: {
+								syntax: "ecmascript",
+								jsx: true
+							},
+							externalHelpers: true,
+							preserveAllComments: false,
+							transform: {
+								react: {
+									runtime: "automatic",
+									throwIfNamespace: true,
+									useBuiltins: false
+								}
+							}
+						}
+					}
+				},
+				type: "javascript/auto"
+			},
+			{
+				test: /.less$/,
+				use: [
+					rspack.CssExtractRspackPlugin.loader,
+					{
+						loader: "css-loader", options: {
+							modules: {
+								namedExport: false
+							}
+						}
+					},
+					"less-loader"
+				],
+			}
+		]
+	},
+	optimization: {
+		minimize: false // Disabling minification because it takes too long on CI
+	},
+	plugins: [
+		new rspack.HtmlRspackPlugin({
+			template: "./index.html"
+		}),
+		new rspack.CssExtractRspackPlugin()
+	],
+	output: {
+		path: path.resolve(__dirname, "dist")
+	},
+	experiments: {
+		css: false,
+	}
+};
+module.exports = config;
