46ac2e94d0c143faa8bd6c104a80ea728a136cf0
neverland <jait.chen@foxmail.com>
2024-04-28 17:32:43 +0800

    feat: add Rsbuild module-federation-enhanced example (#58)

---
+++ rsbuild/module-federation-enhanced/consumer/rsbuild.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { ModuleFederationPlugin } from "@module-federation/enhanced/rspack";
+
+export default defineConfig({
+  server: {
+    port: 2000,
+  },
+  tools: {
+    rspack: (config, { appendPlugins }) => {
+      appendPlugins([
+        new ModuleFederationPlugin({
+          name: "federation_consumer",
+          remotes: {
+            federation_provider:
+              "federation_provider@http://localhost:3000/mf-manifest.json",
+          },
+          shared: ["react", "react-dom"],
+        }),
+      ]);
+    },
+  },
+  plugins: [pluginReact()],
+});
---
+++ rsbuild/module-federation-enhanced/provider/rsbuild.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { ModuleFederationPlugin } from "@module-federation/enhanced/rspack";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  dev: {
+    // It is necessary to configure assetPrefix, and in the production build, you need to configure output.assetPrefix
+    assetPrefix: "http://localhost:3000",
+  },
+  tools: {
+    rspack: (config, { appendPlugins }) => {
+      // You need to set a unique value that is not equal to other applications
+      config.output!.uniqueName = "federation_provider";
+      appendPlugins([
+        new ModuleFederationPlugin({
+          name: "federation_provider",
+          exposes: {
+            "./button": "./src/Button.tsx",
+          },
+          shared: ["react", "react-dom"],
+        }),
+      ]);
+    },
+  },
+  plugins: [pluginReact()],
+});
