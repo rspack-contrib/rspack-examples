edf60279ef9f0c553b6c405724323c93c3fc1c03
neverland <chenjiahan.jait@bytedance.com>
2024-04-28 16:42:02 +0800

    feat: mf enhaced

---
+++ rsbuild/module-federation-enhanced/consumer/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+// The remote component provided by federation_provider
+import ProviderButton from "federation_provider/button";
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+      <div>
+        <ProviderButton />
+      </div>
+    </div>
+  );
+};
+
+export default App;
