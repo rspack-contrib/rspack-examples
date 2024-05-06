73ccfc7f1c17a357f7a17e2d08ff997de69f90ed
chenjiahan <chenjiahan.jait@bytedance.com>
2024-05-06 22:36:10 +0800

    fix(muti-entry): configure multiple HTML plugin

--- rspack/multi-entry/rspack.config.js
+++ rspack/multi-entry/rspack.config.js
@@ -1,14 +1,23 @@
 const rspack = require("@rspack/core");
 /** @type {import('@rspack/cli').Configuration} */
 const config = {
-	mode: "development",
-	entry: {
-		index: "./index.js",
-		second: "./second.js"
-	},
-	output: {
-		publicPath: "http://localhost:3000"
-	},
-	plugins: [new rspack.HtmlRspackPlugin()]
+  mode: "development",
+  entry: {
+    index: "./index.js",
+    second: "./second.js",
+  },
+  output: {
+    publicPath: "http://localhost:3000",
+  },
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      filename: "index.html",
+      chunks: ["index"],
+    }),
+    new rspack.HtmlRspackPlugin({
+      filename: "second.html",
+      chunks: ["second"],
+    }),
+  ],
 };
 module.exports = config;
