1b91a64f8ca64631b1fc1aa4744bf71ebc9029ed
chenjiahan <chenjiahan.jait@bytedance.com>
2024-05-16 21:48:05 +0800

    Add example for React Compiler with Rspack

---
+++ rspack/react-compiler-babel/rspack.config.js
@@ -0,0 +1,60 @@
+const rspack = require('@rspack/core');
+/** @type {import('@rspack/cli').Configuration} */
+const config = {
+  entry: {
+    main: './src/index.jsx',
+  },
+  resolve: {
+    extensions: ['...', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              sourceMap: true,
+              jsc: {
+                parser: {
+                  syntax: 'ecmascript',
+                  jsx: true,
+                },
+                externalHelpers: true,
+                preserveAllComments: false,
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                    throwIfNamespace: true,
+                    useBuiltins: false,
+                  },
+                },
+              },
+            },
+          },
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: ['babel-plugin-react-compiler'],
+              parserOpts: {
+                plugins: ['jsx'],
+              },
+            },
+          },
+        ],
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.(png|svg|jpg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: './index.html',
+    }),
+  ],
+};
+module.exports = config;
