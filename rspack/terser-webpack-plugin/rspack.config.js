959c5cb489f94746e31681fb353a835f29a6eb92
neverland <chenjiahan.jait@bytedance.com>
2024-08-16 16:51:45 +0800

    fix

--- rspack/terser-webpack-plugin/rspack.config.js
+++ rspack/terser-webpack-plugin/rspack.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('@rspack/cli');
-const terserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const { StatsWriterPlugin } = require('webpack-stats-plugin');
 const rspack = require('@rspack/core');
+
 module.exports = defineConfig({
   plugins: [new StatsWriterPlugin()],
   optimization: {
-    minimizer: [new terserPlugin(), new rspack.LightningCssMinimizerRspackPlugin()],
+    minimizer: [new TerserPlugin(), new rspack.LightningCssMinimizerRspackPlugin()],
   },
 });
