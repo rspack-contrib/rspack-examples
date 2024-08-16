5723925147e9e6121bbdc7aeaa728a29faddd85f
neverland <jait.chen@foxmail.com>
2024-08-16 16:54:43 +0800

    fix: bump versions and update deprecated usage (#118)

--- rspack/terser-webpack-plugin/rspack.config.js
+++ rspack/terser-webpack-plugin/rspack.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('@rspack/cli');
-const terserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const { StatsWriterPlugin } = require('webpack-stats-plugin');
 const rspack = require('@rspack/core');
+
 module.exports = defineConfig({
   plugins: [new StatsWriterPlugin()],
   optimization: {
-    minimizer: [new terserPlugin(), new rspack.SwcCssMinimizerRspackPlugin()],
+    minimizer: [new TerserPlugin(), new rspack.LightningCssMinimizerRspackPlugin()],
   },
 });
