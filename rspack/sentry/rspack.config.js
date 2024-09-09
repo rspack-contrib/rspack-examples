bfa534a058a11f2503a03f8f99c16db98af5936f
hardfist <yangjianzju@gmail.com>
2024-09-09 11:25:09 +0800

    feat: add sentry plugin example (#135)

---
+++ rspack/sentry/rspack.config.js
@@ -0,0 +1,13 @@
+const rspack = require('@rspack/core');
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
+/** @type {import('@rspack/cli').Configuration} */
+const config = {
+  entry: './src/index.js',
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: './index.html',
+    }),
+    sentryWebpackPlugin({}),
+  ],
+};
+module.exports = config;
