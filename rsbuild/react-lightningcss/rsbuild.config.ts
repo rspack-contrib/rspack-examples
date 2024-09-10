4dee5b219b0d43ea2af753e1e72ff655c879cea8
neverland <jait.chen@foxmail.com>
2024-09-10 15:44:00 +0800

    fix: remove deprecated plugin-lightningcss (#141)

--- rsbuild/react-lightningcss/rsbuild.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginLightningcss } from '@rsbuild/plugin-lightningcss';
-
-export default defineConfig({
-  plugins: [pluginReact(), pluginLightningcss()],
-});
