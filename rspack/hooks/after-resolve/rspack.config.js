6e47f9d715934611f214627c716430e6d3bb3a72
hardfist <yangjianzju@gmail.com>
2024-09-24 18:03:36 +0800

    feat: add afterResolve hook usage (#143)

---
+++ rspack/hooks/after-resolve/rspack.config.js
@@ -0,0 +1,24 @@
+const rspack = require('@rspack/core');
+/** @type {import('@rspack/cli').Configuration} */
+const config = {
+  entry: './src/index.js',
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: './index.html',
+    }),
+    {
+      /**
+       *
+       * @param {import('@rspack/core').Compiler} compiler
+       */
+      apply(compiler) {
+        compiler.hooks.normalModuleFactory.tap('nmf', (nmf) => {
+          nmf.hooks.afterResolve.tap('afterResolver', (data) => {
+            console.log('data:', data.createData?.resource);
+          });
+        });
+      },
+    },
+  ],
+};
+module.exports = config;
