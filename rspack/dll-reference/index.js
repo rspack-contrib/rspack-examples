36a148d5830c6171a2eeab6e3cb4d28b16a701da
qixuan <58852732+GiveMe-A-Name@users.noreply.github.com>
2024-11-15 14:33:20 +0800

    feat: add dll & dll-reference plugin examples (#169)
    Co-authored-by: 谢奇璇 <xieqixuan.xx@bytedance.com>

---
+++ rspack/dll-reference/index.js
@@ -0,0 +1,14 @@
+import a from '../dll/a';
+import alpha from '../dll/alpha';
+import beta from 'beta/beta'
+import b from 'beta/b'
+import c from 'beta/c'
+import _ from 'lodash';
+
+
+console.log(a)
+console.log(alpha)
+console.log(beta)
+console.log(b)
+console.log(c)
+console.log(_.sum([1, 2]))
