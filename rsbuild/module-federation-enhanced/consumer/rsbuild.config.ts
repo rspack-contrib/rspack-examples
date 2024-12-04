6ecd383d0ea17f03d341a66046e0e0b921787cb3
neverland <jait.chen@foxmail.com>
2024-12-04 14:43:14 +0800

    feat: update Rsbuild MF examples (#185)

--- rsbuild/module-federation-enhanced/consumer/rsbuild.config.ts
+++ rsbuild/module-federation-enhanced/consumer/rsbuild.config.ts
@@ -1,27 +1,19 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
-import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 
 export default defineConfig({
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: 'federation_consumer',
+      remotes: {
+        federation_provider: 'federation_provider@http://localhost:3000/mf-manifest.json',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
   server: {
     port: 2000,
   },
-  tools: {
-    rspack: (config, { appendPlugins }) => {
-      appendPlugins([
-        new ModuleFederationPlugin({
-          name: 'federation_consumer',
-          remotes: {
-            federation_provider: 'federation_provider@http://localhost:3000/mf-manifest.json',
-          },
-          shared: ['react', 'react-dom'],
-        }),
-      ]);
-    },
-  },
-  plugins: [
-    pluginReact({
-      splitChunks: { react: false, router: false },
-    }),
-  ],
 });
