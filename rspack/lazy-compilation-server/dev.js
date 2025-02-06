fb5fdeb133c0571d5349e6d357184d664cc394ec
jserfeng <1114550440@qq.com>
2025-02-06 16:06:23 +0800

    fix: correct package.json

--- rspack/lazy-compilation-server/dev.js
+++ rspack/lazy-compilation-server/dev.js
@@ -1,11 +1,10 @@
 const rspack = require('@rspack/core');
 const { RspackDevServer } = require('@rspack/dev-server');
-const Html = require('html-webpack-plugin');
 
 const compiler = rspack({
   entry: './src/index.js',
   context: __dirname,
-  plugins: [new Html()],
+  plugins: [new rspack.HtmlRspackPlugin()],
   experiments: {
     lazyCompilation: {
       backend: {
