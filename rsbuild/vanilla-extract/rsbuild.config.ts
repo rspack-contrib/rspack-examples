47deb759aeacdc9ea6b1a56a924ca69e0220eb04
Andrey Litvinov <drune.email@gmail.com>
2025-02-22 16:03:57 +0300

    fix: reactRefreshOptions exclude must contain node_modules (#242)

--- rsbuild/vanilla-extract/rsbuild.config.ts
+++ rsbuild/vanilla-extract/rsbuild.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     pluginReact({
       reactRefreshOptions: {
-        exclude: [/\.css\.ts$/],
+        exclude: [/node_modules/, /\.css\.ts$/],
       },
     }),
   ],
